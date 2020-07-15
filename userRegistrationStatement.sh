f55f82c2b06edfa180eebf18df4a9e94350b3471
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-15 16:25:35 +0530

    Added program to validate users lastname

--- userRegistrationStatement.sh
+++ userRegistrationStatement.sh
@@ -17,3 +17,24 @@ function firstName {
         done
 }
 firstName
+
+function lastName {
+        shopt -s extglob
+        flag=0
+        lastNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+        while [[ $flag -eq 0 ]]
+        do
+                echo "Enter your last name : "
+                read lastName
+                if [[ $lastName =~ $lastNamePattern ]]
+                then
+                        echo valid last name
+                        ((flag++))
+                        break
+                else
+                        echo not valid last name. First letter should be CAPITAL
+                fi
+        done
+}
+lastName
+
